1424bbcaf2d0058428bd41b4569de9cd58f55593
Lucas Abbade <lucasabbade@hotmail.com>
2019-06-26 22:06:12 -0300

    finish frontend

--- frontend/public/js/main.js
+++ frontend/public/js/main.js
@@ -2,6 +2,10 @@ function disable_refresh_btn(state) {
     $('#refresh_btn').prop('disabled', state);
 }
 
+function disable_get_btn(state) {
+    $('#get_lote').prop('disabled', state);
+}
+
 function make_lotes_options(arr) {
     var options = '<option value="_placeholder">Selecione o Lote</option>';
     for (var i = 0; i < arr.length; i++) {
@@ -25,9 +29,38 @@ function get_lotes() {
     });
 }
 
+function make_lote_table(data) {
+    var table = "";
+    for (var i = 0; i < data.length; i++) {
+        table += "<tr>\n";
+        table += `<td>${data[i]._id}</td>\n`;
+        table += `<td>${data[i].lote}</td>\n`;
+        table += `<td>${data[i].conteudo}</td>\n`;
+        table += `<td>${data[i].validade}</td>\n`; 
+        table += "</tr>\n";
+    }
+    return table;
+}
+
 function get_single_lote() {
-    // TODO
-    // get every toddinho from specified lote
+    const lote = $('#lotes').val();
+
+    if (lote === '_placeholder') {
+        alert('Por favor, selecione um lote');
+    } else {
+        $.ajax({
+            url: `http://localhost:5000/toddy/listar?lote=${lote}`,
+            beforeSend: () => {
+                disable_get_btn(true);
+            }
+        }).done(data => {
+            $("#lote-table-body").html(make_lote_table(data));
+        }).fail(() => {
+            alert(`Não foi possível buscar o lote ${lote}`);
+        }).always(() => {
+            disable_get_btn(false);
+        });
+    }
 }
 
 $(document).ready(() => {
