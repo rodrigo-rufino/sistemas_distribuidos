75913c4b649e3dc1593dc214229e486b78bf7b38
Lucas Abbade <lucasabbade@hotmail.com>
2019-06-29 13:38:39 -0300

    connection with DB working

--- middleware/dao.js
+++ middleware/dao.js
@@ -1,85 +1,32 @@
-var mongojs = require('mongojs');
+var mysql = require('mysql');
 
-var db = mongojs('mongo:27017/aula', ['toddy']);
+var con = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'ec021_av2_musicfy'
+};
 
-const o_id = mongojs.ObjectID;
-const base_filter = get_attr => fix_attr => compareTo => f => res => f(fix_attr(get_attr(res)), compareTo);
+function run_query(query, callback) {
+    var connection = mysql.createConnection(con);
+    connection.connect();
+
+    console.log(`Running query "${query}" in db`);
+
+    connection.query(query, function (err, rows, fields) {
+        if (err) {
+            console.log(`Erro na query: ${err}`);
+            return []
+        } else {
+            callback(rows);
+        }
+    });
+
+    connection.end();
+}
 
 module.exports = {
-    inserir: (toddy, callback) => {
-        db.toddy.save(toddy, (err, results) => {
-            if (err) {
-                const err_text = `Error inserting:\n${err}`;
-                console.log(err_text);
-                callback(err_text);
-            } else {
-                const res_text = `Created object id: ${results._id}`;
-                console.log(res_text);
-                callback(res_text);
-            }
-        });
-    },
-    atualizar: (_id, toddy, callback) => {
-        db.toddy.update({ _id: o_id(_id) }, { "$set": toddy }, (err, results) => {
-            if (err) {
-                const err_text = `Error updating:\n${err}`;
-                console.log(err_text);
-                callback(err_text);
-            } else {
-                callback(results);
-            }
-        });
-    },
-    listar: (_id, _vencidos, has_vencidos, _lote, callback) => {
-        db.toddy.find(_id ? { _id: o_id(_id) } : {}, (error, results) => {
-            if (error) {
-                const error_text = `Error getting toddies:\n${error}`;
-                console.log(error_text);
-                callback(error_text);
-            } else {
-                if (has_vencidos) {
-                    results = results.filter(base_filter(r => r.validade)
-                        (d => new Date(d.split('/').reverse()))
-                        (new Date())
-                        ((d1, d2) => _vencidos ? d1 < d2 : d1 > d2));
-                }
-                if (_lote !== undefined) {
-                    results = results.filter(r => r.lote === _lote);
-                }
-                callback(results);
-            }
-        });
-    },
-    listarPorLote: (lote, callback) => {
-        db.toddy.find({lote}, (error, results) => {
-            if (error) {
-                console.log(`Error getting lote ${lote}: ${error}`);
-                callback([]);
-            } else {
-                callback(results);
-            }
-        });
-    },
-    listarLotes: callback => {
-        db.toddy.distinct("lote", {}, (error, results) => {
-            if (error) {
-                const err_text = `Error getting lotes:\n${err}`;
-                console.log(err_text);
-                callback(err_text);
-            } else {
-                callback(results);
-            }
-        });
-    },
-    excluir: (_id, callback) => {
-        db.toddy.remove({ _id: o_id(_id) }, function (error, results) {
-			if (error) {
-				const error_text = `Error deleting toddy ${_id}:\n${error}`;
-				console.log(error_text);
-				callback(error_text);
-			} else {
-				callback(results)
-			}
-		});
+    test: (callback) => {
+        run_query('SELECT * FROM musica;', callback);
     }
 }
--- middleware/index.js
+++ middleware/index.js
@@ -2,66 +2,7 @@ var restify = require('restify');
 var dao = require('./dao');
 var corsMiddleware = require('restify-cors-middleware');
 
-const default_db_res = res => res_text => res.send(res_text);
-
-function inserir(req, res, next) {
-	const toddy = {
-		lote: req.body.lote,
-		conteudo: req.body.conteudo,
-		validade: req.body.validade
-	};
-
-	console.log(`[inserir] Created toddy: ${JSON.stringify(toddy)}`);
-
-	dao.inserir(toddy, default_db_res(res));
-	next();
-}
-
-function atualizar(req, res, next) {
-	const _id = req.query.id;
-	const toddy = {
-		lote: req.body.lote,
-		conteudo: req.body.conteudo,
-		validade: req.body.validade
-	}
-
-	dao.atualizar(_id, toddy, default_db_res(res));
-	next();
-}
-
-function listar(req, res, next) {
-	const _id = req.query.id;
-	const _vencidos = req.query.vencidos === 'true';
-	const has_vencidos = req.query.vencidos !== undefined;
-	const _lote = req.query.lote;
-	const has_lote = _lote !== undefined;
-	const handle_res = default_db_res(res);
-
-	if (has_lote) {
-		dao.listarPorLote(_lote, handle_res);
-	} else {
-		dao.listar(_id, _vencidos, has_vencidos, _lote, handle_res);
-	}
-	next();
-}
-
-function listarLotes(req, res, next) {
-	dao.listarLotes(default_db_res(res));
-	next();
-}
-
-function excluir(req, res, next) {
-	const _id = req.query.id;
-
-	if (_id === undefined) {
-		res.send("Você precisa especificar o ID a ser apagado");
-	} else {
-		dao.excluir(_id, default_db_res(res));
-	}
-	next();
-}
-
-var server = restify.createServer({ name: 'EC021 - Prática 2' });
+var server = restify.createServer({ name: 'Prova 2' });
 
 server.use(restify.plugins.bodyParser());
 server.use(restify.plugins.queryParser());
@@ -75,12 +16,14 @@ const cors = corsMiddleware({
 server.pre(cors.preflight);
 server.use(cors.actual);
 
-var toddyPoint = '/toddy';
-server.post(toddyPoint + '/salvar', inserir);
-server.put(toddyPoint + '/salvar', atualizar);
-server.get(toddyPoint + '/listar', listar);
-server.get(toddyPoint + '/listarLotes', listarLotes);
-server.del(toddyPoint + '/excluir', excluir);
+server.get('/', (req, res, next) => {
+	res.send('hello world');
+	next();
+});
+
+server.get('/get_musicas', (req, res, next) => {
+	dao.test((data) => res.send(data));
+});
 
 const PORT = 5000;
 
