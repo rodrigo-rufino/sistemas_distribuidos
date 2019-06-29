daaa17b610140de2f34d59082057cf7a3dc1571b
Lucas Abbade <lucasabbade@hotmail.com>
2019-06-29 14:27:14 -0300

    fixes jquery ajax

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,60 +1,54 @@
 var mid = 'http://localhost:5000/musicfy/';
 
-$(document).ready(function() {
+function disable_box(state) {
+    $("#genero").attr("disabled", state);
+}
+
+$(document).ready(function () {
+    console.log('ready');
+
     $.ajax({
         url: mid + 'listarGeneros',
-        type: 'GET',
-        success: function(result) {
-            $.each(result, function(indice, genero) {
-                $("#genero").append(`<option value="` + genero.descricao + `">` + genero.descricao + `</option>`);
-            });
-        },
-        error: function() {
-            alert('Houve um erro.');
-        },
-        complete: function() {
-            $('#genero').formSelect();
+        beforeSend: () => {
+            disable_box(true);
         }
-    })
-});
+    }).done(result => {
+        console.log(`received results: ${JSON.stringify(result)}`);
+        $.each(result, function (indice, genero) {
+            $("#genero").append(`<option value="` + genero.descricao + `">` + genero.descricao + `</option>`);
+        });
+    }).fail((err) => {
+        alert(`Erro: ${JSON.stringify(err)}`);
+    }).always(() => {
+        disable_box(false);
+    });
 
-$("#genero").change(function() {
-    //$("#tabela > tbody").empty();
-    $("#lista").empty();
+    $("#genero").change(() => {
+        //$("#tabela > tbody").empty();
+        $("#lista").empty();
 
-    var genero_id = this.value;
-    
-    $.ajax({
-        url: mid + 'buscarMusicaPorGenero?genero_id=' + genero_id,
-        type: 'GET',
-        beforeSend: function() {
-            $("#genero").attr("disabled", true);
-        },
-        success: function(result) {
-            /*$.each(result, function(indice, musica) {
-                $("#tabela > tbody").append(`<tr>`
-                    +`<td>`+ musica.musica_id + `</td>`
-                    +`<td>`+ musica.titulo + `ml</td>`
-                    +`<td>`+ musica.artista + `</td>`
-                    +`</tr>`);
-            });*/
+        var genero_id = this.value;
 
-            $.each(result, function(indice, musica) {
+        $.ajax({
+            url: mid + 'buscarMusicaPorGenero?genero_id=' + genero_id,
+            beforeSend: () => {
+                disable_box(true);
+            }
+        }).done(result => {
+            console.log(`received results: ${JSON.stringify(result)}`);
+            $.each(result, function (indice, musica) {
                 $("#lista").append(`<ul>`
-                    +`<li>`+ musica.musica_id + `</td>`
-                        + `<ul>`
-                        +`<li>`+ musica.titulo + `ml</td>`
-                        +`<li>`+ musica.artista + `</td>`
-                        +`</ul>`
-                    +`</ul>`);
+                    + `<li>` + musica.musica_id + `</td>`
+                    + `<ul>`
+                    + `<li>` + musica.titulo + `ml</td>`
+                    + `<li>` + musica.artista + `</td>`
+                    + `</ul>`
+                    + `</ul>`);
             });
-
-        },
-        error: function() {
-            alert('Houve um erro.');
-        },
-        complete: function() {
-            $("#genero").attr("disabled", false);
-        }
-    })
+        }).fail((err) => {
+            alert(`Erro: ${JSON.stringify(err)}`);
+        }).always(() => {
+            disable_box(false);
+        });
+    });
 });
