3bc67cde7822301d88d3dd0034b60e4e0d4a807d
Lucas Abbade <lucasabbade@hotmail.com>
2019-06-29 14:20:21 -0300

    changed dao

--- middleware/dao.js
+++ middleware/dao.js
@@ -7,7 +7,7 @@ var con = {
     database: 'ec021_av2_musicfy'
 };
 
-function run_query(query, callback) {
+function runQuery(query, callback) {
     var connection = mysql.createConnection(con);
     connection.connect();
 
@@ -26,7 +26,13 @@ function run_query(query, callback) {
 }
 
 module.exports = {
-    test: (callback) => {
-        run_query('SELECT * FROM musica;', callback);
+    getMusicas: (callback) => {
+        runQuery('SELECT * FROM musica;', callback);
+    },
+    listarGeneros: (callback) => {
+        runQuery('SELECT * FROM genero;', callback);
+    },
+    getMusicasByGenero: (genero_id, callback) => {
+        runQuery(`SELECT * FROM musica WHERE genero_id = ${genero_id};`, callback);
     }
 }
